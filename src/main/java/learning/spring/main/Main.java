507cb70e6f27b5736ebfa13d839abbc2059bc9e8
Ashish Kumar <ashishkumardakh@gmail.com>
2026-08-13 12:48:04 +0530

    Initial commit

---
+++ src/main/java/learning/spring/main/Main.java
@@ -0,0 +1,15 @@
+package learning.spring.main;
+
+
+//Step:1  add dependencies ( mvnreposatory :- spring core & spring context) 
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		
+		
+	}
+
+}
