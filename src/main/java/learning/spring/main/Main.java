46b336281acba5f1e525d310a9925567ba0e3a16
Ashish Kumar <ashishkumardakh@gmail.com>
2026-08-13 13:12:04 +0530

    update

--- src/main/java/learning/spring/main/Main.java
+++ src/main/java/learning/spring/main/Main.java
@@ -1,12 +1,21 @@
 package learning.spring.main;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import learning.spring.model.Employee;
 
 //Step:1  add dependencies ( mvnreposatory :- spring core & spring context) 
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		
+		ApplicationContext ioc = new ClassPathXmlApplicationContext("application-context.xml");
+		Employee bean = ioc.getBean(Employee.class);
+		
+		System.out.println(bean);
+		
 		
 		
 		
